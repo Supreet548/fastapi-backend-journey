c7c5689425d69ddfbc2c69a6a499769a2014c7f6
Supreet Kumar <supreetkumar548@gmail.com>
2026-05-11 20:07:33 +0530

    Added POST feature

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
-
+from pydantic import BaseModel
 app = FastAPI()
+
+class Student(BaseModel):
+    name:str
+    marks:int
+
+
 
 @app.get("/")
 def home():
@@ -9,3 +15,20 @@ def home():
 @app.get("/about")
 def about():
     return {"message": "Learning FastAPI"}
+
+
+@app.get("/student/{student_id}")
+def get_student(student_id:int):
+    return {"student_id":student_id}
+
+@app.get("/search")
+def search(name:str):
+    return{"student_name":name}
+
+@app.post("/students")
+def create_student(student:Student):
+    return{
+        "message":"Student created",
+        "student": student
+
+    }
