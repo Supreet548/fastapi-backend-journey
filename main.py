c98961b9e0efc8fba1785feb5f7356f3645d04de
Supreet Kumar <supreetkumar548@gmail.com>
2026-05-12 20:50:17 +0530

    Day 3 FastAPI CRUD practice

--- main.py
+++ main.py
@@ -1,34 +1,55 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+
 app = FastAPI()
 
+students = []
+
 class Student(BaseModel):
-    name:str
-    marks:int
+    name : str
+    marks : int
 
 
-
-@app.get("/")
-def home():
-    return {"message": "Hello AI Engineer"}
-
-@app.get("/about")
-def about():
-    return {"message": "Learning FastAPI"}
-
-
-@app.get("/student/{student_id}")
-def get_student(student_id:int):
-    return {"student_id":student_id}
-
-@app.get("/search")
-def search(name:str):
-    return{"student_name":name}
+#CREATE
 
 @app.post("/students")
-def create_student(student:Student):
+def create_students(student:Student):
+    students.append(student.model_dump())
     return{
-        "message":"Student created",
-        "student": student
+        "message":"Student added",
+        "students":students
+    }
 
+#READ
+
+@app.get("/students")
+def get_students():
+    return students
+
+#UPDATE
+
+@app.put("/students/{student_id}")
+def update_student(student_id:int, updated_student:Student):
+    if student_id>=len(students):
+        return{"error":"Student not found"}
+    
+    students[student_id]=updated_student.model_dump()
+
+    return{
+        "message":"Student updated",
+        "students":students
+    }
+
+#DELETE
+
+@app.delete("/students/{student_id}")
+def delete_student(student_id:int):
+    if student_id>=len(students):
+        return{"error":"Student not found"}
+    
+    deleted_student = students.pop(student_id)
+
+    return{
+        "message":"Student deleted",
+        "students":deleted_student
     }
